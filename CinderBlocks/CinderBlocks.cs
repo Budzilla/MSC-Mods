f8b7124511591cc0c73bd1b890807b4bc58f4d16
Budzilla <33190569+Budzilla@users.noreply.github.com>
2018-10-29 13:53:57 -0600

    Initial commit

---
+++ CinderBlocks/CinderBlocks.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using MSCLoader;
+using UnityEngine;
+
+namespace CinderBlocks
+{
+    public class CinderBlocks : Mod
+    {
+        public override string ID => "CinderBlocks";
+        public override string Name => "Cinder Blocks";
+        public override string Author => "Budzilla";
+        public override string Version => "1.0";
+        public override bool UseAssetsFolder => true;
+
+        private static readonly List<Vector3> _pos = new List<Vector3>
+        {
+            new Vector3(51.563f, -1.349f, -88.717f),
+            new Vector3(51.778f, -1.349f, -88.720f),
+            new Vector3(51.989f, -1.349f, -88.724f),
+            new Vector3(52.204f, -1.349f, -88.728f)
+        };
+
+        private List<GameObject> _list;
+        private GameObject _block;
+
+        public CinderBlocks()
+        {
+            CinderBlocksSaveData.SavePath = System.IO.Path.Combine(Application.persistentDataPath, "cinderblocks.xml");
+        }
+
+        public void InitSpawn()
+        {  
+            _list = new List<GameObject>();
+            for (int i = 0; i < 4; ++i)
+            {
+                GameObject cinderblock = GameObject.Instantiate<GameObject>(_block);
+                cinderblock.name = "cinder block(Clone)";
+                cinderblock.tag = "PART";
+                cinderblock.layer = LayerMask.NameToLayer("Parts");
+                cinderblock.transform.position = _pos[i];
+                cinderblock.transform.rotation = Quaternion.Euler(270.0f, 90.0f, 0.0f);
+                cinderblock.GetComponent<Rigidbody>().isKinematic = true;
+                _list.Add(cinderblock);
+            }
+            GameObject.Destroy(_block);
+        }
+
+        public override void OnLoad()
+        {
+            AssetBundle ab = LoadAssets.LoadBundle(this, "cinderblocks.unity3d");
+            GameObject original = ab.LoadAsset<GameObject>("cinderblock.prefab");
+
+            _block = GameObject.Instantiate<GameObject>(original);
+            _block.name = "cinder block";
+            _block.tag = "PART";
+            _block.layer = LayerMask.NameToLayer("Parts");
+            _block.GetComponent<Rigidbody>().isKinematic = true;
+
+            GameObject.Destroy(original);
+            ab.Unload(false);
+
+            // Load save
+            if (CinderBlocksSaveData.saveExists == true)
+            {
+                CinderBlocksSaveData data = CinderBlocksSaveData.Deserialize<CinderBlocksSaveData>(CinderBlocksSaveData.SavePath);
+                for (int i = 0; i < data.Pos.Count; ++i)
+                {
+                    GameObject cinderblock = GameObject.Instantiate<GameObject>(_block);
+                    cinderblock.transform.position = data.Pos[i];
+                    cinderblock.transform.rotation = data.Rot[i];
+                }
+            }
+
+            // Initialize
+            if (CinderBlocksSaveData.saveExists == false)
+            {
+                InitSpawn();
+            }
+        }
+
+        public override void OnSave()
+        {
+            {
+                CinderBlocksSaveData data = new CinderBlocksSaveData(new List<Vector3>(), new List<Quaternion>());
+                foreach (GameObject obj in GameObject.FindObjectsOfType<GameObject>())
+                    if (obj.name == "cinder block(Clone)")
+                    {
+                        data.Pos.Add(obj.transform.position);
+                        data.Rot.Add(obj.transform.rotation);
+                    }
+                    CinderBlocksSaveData.Serialize(data, CinderBlocksSaveData.SavePath);
+            }
+        }
+    }
+}
---
+++ CinderBlocks/CinderBlocksSaveData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using MSCLoader;
+using UnityEngine;
+
+namespace CinderBlocks
+{
+    public class CinderBlocksSaveData
+    {
+        public List<Vector3> Pos;
+        public List<Quaternion> Rot;
+
+        public CinderBlocksSaveData()
+        {
+            Pos = new List<Vector3>();
+            Rot = new List<Quaternion>();
+        }
+
+        public CinderBlocksSaveData(List<Vector3> pos, List<Quaternion> rot)
+        {
+            this.Pos = pos;
+            this.Rot = rot;
+        }
+
+        public static string SavePath;
+        public static bool saveExists = false;
+
+        public static void Serialize<T>(T data, string path)
+        {
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                XmlWriter writer = XmlWriter.Create(new StreamWriter(path));
+                serializer.Serialize(writer, data);
+                writer.Close();
+                saveExists = true;
+            }
+            catch (Exception ex)
+            {
+                ModConsole.Error(ex.ToString());
+            }
+        }
+
+        public static T Deserialize<T>(string path) where T : new()
+        {
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                XmlReader reader = XmlReader.Create(new StreamReader(path));
+                return (T)serializer.Deserialize(reader);
+            }
+            catch { }
+            return new T();
+        }
+    }
+}
